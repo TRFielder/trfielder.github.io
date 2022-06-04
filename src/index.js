9d0980ad1d22b543fa22c06c859a6458b0bfbbb2
Tom Fielder <tom.fielder97@gmail.com>
2022-06-05 00:16:39 +0100

    Add non-functional contact form

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import "./styles/globalStyle.css";
 import "./styles/Hero.css";
 import "./styles/AboutMe.css";
 import "./styles/Projects.css";
+import "./styles/Contact.css";
 import "./utilities/setupAnchors";
