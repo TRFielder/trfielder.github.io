3d7775ef3564f9433b42eb6a52692359dec96cd4
Tom Fielder <tom.fielder@npl.co.uk>
2024-02-20 22:10:38 +0000

    Remove dev icons

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -5,62 +5,17 @@ const About = () => {
     <section id="About" className="flex flex-col items-center font-extralight">
       <article className="text-white flex flex-col items-center justify-center py-12 w-1/2">
         <h3 className="text-2xl font-semibold mb-8">About Me</h3>
-        <div className="flex flex-row">
+        <div className="flex flex-col">
           <p className="my-5 mx-0 text-xl">
             I am currently working as a front end developer as part of a growing
             team building a web-based platform for scientific applications.
           </p>
-          <div className="grid grid-cols-4 gap-2">
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
-              width={250}
-              height={250}
-              className="bg-white"
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
-              width={250}
-              height={250}
-            />
-            <Image
-              alt=""
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
-              width={250}
-              height={250}
-            />
-          </div>
+
+          <p className="my-5 mx-0 text-xl">
+            Outside that, I&apos;m into building small hobby projects (both
+            software and physical), and enjoy pretending I&apos;m good at moving
+            heavy circles at the local gym
+          </p>
         </div>
       </article>
     </section>
